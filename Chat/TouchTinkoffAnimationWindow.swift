00c03ace0e6acee57acb966cff3eca03493edd92
Vladimir Bolotov <bolotov.va@ya.ru>
2021-04-29 23:42:20 +0300

    Add touch animation window

---
+++ Chat/TouchTinkoffAnimationWindow.swift
@@ -0,0 +1,20 @@
+//
+//  TouchTinkoffAnimationWindow.swift
+//  Chat
+//
+//  Created by VB on 29.04.2021.
+//
+
+import UIKit
+
+class TouchTinkoffAnimationWindow: UIWindow {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
